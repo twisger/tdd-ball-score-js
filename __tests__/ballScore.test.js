eb7151aa1874ee9eeb7e042c0b7d34e5edead4d7
Fulin Li <fulin.li@thoughtworks.com>
2020-05-31 18:12:08 +0800

    should score be 30 then throw ball given first ball 10, second ball 10, third ball 10 in ten round

--- __tests__/ballScore.test.js
+++ __tests__/ballScore.test.js
@@ -11,3 +11,7 @@ test('should score be 20 when throw ball given first ball 5, second ball 5, thir
 test('should score be 8 when throw ball given first ball 5, second ball 3, third ball 10 in second round', () => {
   expect(calculateRoundScore([5, 3, 10], 2)).toBe(8);
 });
+
+test('should score be 30 then throw ball given first ball 10, second ball 10, third ball 10 in ten round', () => {
+  expect(calculateRoundScore([10, 10, 10], 10)).toBe(30);
+});
