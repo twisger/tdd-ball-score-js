ebb5e7b4e82a263232fe08f1530e0f7e390d3fbe
Fulin Li <fulin.li@thoughtworks.com>
2020-05-31 18:08:15 +0800

    should score be 20 when throw ball given first ball 5, second ball 5, third ball 10 in second round

--- __tests__/ballScore.test.js
+++ __tests__/ballScore.test.js
@@ -3,3 +3,7 @@ import { calculateRoundScore } from '../task/ballScore';
 test('should score be 25 when throw ball given first ball 10, second ball 10, third ball 5 in first round', () => {
   expect(calculateRoundScore([10, 10, 5], 1)).toBe(25);
 });
+
+test('should score be 20 when throw ball given first ball 5, second ball 5, third ball 10 in second round', () => {
+  expect(calculateRoundScore([5, 5, 10], 2)).toBe(20);
+});
--- task/ballScore.js
+++ task/ballScore.js
@@ -2,4 +2,7 @@ export const calculateRoundScore = (scoreList, roundNumber) => {
   if (scoreList[0] === 10) {
     return scoreList.reduce((pre, cur) => pre + cur);
   }
+  if (scoreList[0] < 10 && scoreList[0] + scoreList[1] === 10) {
+    return scoreList.reduce((pre, cur) => pre + cur);
+  }
 };
