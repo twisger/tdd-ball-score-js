80d368554f8d55619aadf6bc01b689d787224745
Fulin Li <fulin.li@thoughtworks.com>
2020-06-04 14:57:33 +0800

    add assert of show message

--- __tests__/ballScore.test.js
+++ __tests__/ballScore.test.js
@@ -1,15 +1,20 @@
-import { calculateRoundScore, calculateTotalScore } from '../src/ballScore';
+import {
+  calculateRoundScore, calculateTotalScore, showRoundMessage,
+} from '../src/ballScore';
 
 test('should score be 25 when throw ball given first ball 10, second ball 10, third ball 5', () => {
   expect(calculateRoundScore([10, 10, 5])).toBe(25);
+  expect(showRoundMessage([10, 10, 5])).toBe('X');
 });
 
 test('should score be 20 when throw ball given first ball 5, second ball 5, third ball 10', () => {
   expect(calculateRoundScore([5, 5, 10])).toBe(20);
+  expect(showRoundMessage([5, 5, 10])).toBe('/');
 });
 
 test('should score be 8 when throw ball given first ball 5, second ball 3, third ball 10', () => {
   expect(calculateRoundScore([5, 3, 10])).toBe(8);
+  expect(showRoundMessage([5, 3, 10])).toBeUndefined();
 });
 
 test('should score be sum value when calculate total score given every round score', () => {
--- src/ballScore.js
+++ src/ballScore.js
@@ -1,3 +1,12 @@
+export const showRoundMessage = (scoreList) => {
+  if (scoreList[0] === 10) {
+    return 'X';
+  }
+  if (scoreList[0] < 10 && scoreList[0] + scoreList[1] === 10) {
+    return '/';
+  }
+};
+
 export const calculateRoundScore = (scoreList) => {
   if (scoreList[0] === 10) {
     return scoreList.reduce((pre, cur) => pre + cur);
